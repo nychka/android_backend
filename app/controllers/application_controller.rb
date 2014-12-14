e5de192d825cf335bd87cb8c3ccdfca28282cd6d
nychos <nychka08@yandex.ru>
2014-12-14 23:54:08 +0200

    add helper method available locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #helper_method :current_user
+  helper_method :available_locales
 
   private
-
+  def available_locales;  [:en, :ua] ; end
   # def current_user
   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
   # end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,2 @@
 module ApplicationHelper
-   def available_locales;  [:en, :ua] ; end
 end
